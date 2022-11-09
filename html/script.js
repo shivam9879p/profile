730fa0a3fb8515607725bd8c30da98dd5c8e15bf
shivam9879p <shivampandey9811@gmail.com>
2022-11-09 15:48:58 +0530

    first commit

---
+++ html/script.js
@@ -0,0 +1,15 @@
+// Typing Animations
+
+var typed = new Typed(".typing", {
+    strings: ["BTECH Student", "JAVA PROGRAMMER", "FRONT END DEVELOPER"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+
+var typed = new Typed(".typing-2", {
+    strings:["BTECH Student", "JAVA PROGRAMMER", "FRONT END DEVELOPER"],
+    typeSpeed:99,
+    backSpeed:58,
+    loop:true
+})
